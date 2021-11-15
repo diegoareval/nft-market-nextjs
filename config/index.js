543ec4ef1ca421e470397a68f0456e0d808d9909
Diego <diego2000avelar@gmail.com>
2021-11-14 19:27:24 -0600

    quicly refactor

--- config/index.js
+++ config/index.js
@@ -1,4 +1,9 @@
 
-const projectId = 'd5b54fa562cb45aba0a9fd370c3abe88';
+ const projectId = 'd5b54fa562cb45aba0a9fd370c3abe88';
+ const nftAddress = '';
+ const nftMarketAddress = '';
 
-module.exports = {projectId}
+module.exports = {
+    projectId, nftAddress, nftMarketAddress
+}
+
